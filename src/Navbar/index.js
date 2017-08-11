59d59aab9b43f2777995c47f8698e3fd642065c1
Chris Mendoza <cmend85@gmail.com>
2017-08-10 23:17:57 -0500

    Added semantic ui react package

---
+++ src/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button, Segment} from 'semantic-ui-react'
+
+const NavBar = () => {
+  <Segment>
+    <Button content="test" />
+  </Segment>
+}
+
+export default NavBar

df46255ed2e2e38022991576bf678f9f8acad50a
Chris Mendoza <cmend85@gmail.com>
2017-09-02 14:47:47 -0500

    Added resume as jpg

--- src/ResumeModal/index.js
+++ src/ResumeModal/index.js
@@ -1,5 +1,7 @@
-import {Image, Modal} from 'semantic-ui-react'
+import {Container, Image, Modal} from 'semantic-ui-react'
 import React, {Component} from 'react'
+
+import MyImage from '../images/Chris-Mendoza-Resume-copy.jpg'
 
 class ResumeModal extends Component {
 
@@ -9,7 +11,9 @@ class ResumeModal extends Component {
     return (
       <Modal closeIcon="close" onClose={closeResumeModal} open={true}>
         <Modal.Content image>
-           <Image src="images/Chris-Mendoza-Resume.pdf" size="huge"/>
+          <Container style={{height:1300, width:875}}>
+            <Image src={MyImage} size="huge"/>
+          </Container>
         </Modal.Content>
       </Modal>
     )
